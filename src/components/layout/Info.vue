46ac4fff16c0628125a218962a327c0dbc980cc9
Iván García <ivgarciag@yahoo.es>
2021-12-02 17:49:23 +0100

    Se añade QR y enlace de play store

--- src/components/layout/Info.vue
+++ src/components/layout/Info.vue
@@ -3,17 +3,18 @@
         <v-card outlined>
             <v-list-item three-line>
                 <v-list-item-content>
-                    <div class="text-overline mb-4">
-                    Aplicación web de escritorio de la HOJA DEL MES
-                    </div>
+                    <div class="text-overline mb-4">Aplicación web de escritorio de la HOJA DEL MES</div>
                     <v-img max-height="350" max-width="500" src="../../img/ic_logo.png"></v-img>
-                    <v-list-item-subtitle>También disponible para móvil</v-list-item-subtitle>
+                    <v-list-item-subtitle>También disponible para móvil. Descárgala en tu Android.</v-list-item-subtitle>
                 </v-list-item-content>
-                <v-list-item-avatar tile size="150" color="grey">QR</v-list-item-avatar>
+                <v-list-item-avatar tile size="150" color="grey">
+                    <v-img src="../../img/qrcode_play.google.com.png"></v-img>
+                </v-list-item-avatar>
+                
             </v-list-item>
 
             <v-card-actions>
-            <v-btn outlined>Play Store</v-btn>
+            <a href="https://play.google.com/store/apps/details?id=com.ivione93.hojames" style="color:#fff;text-decoration:none" target="_blank"><v-img max-height="100" max-width="200" src="../../img/play_store.png"></v-img></a>
             </v-card-actions>
         </v-card>
     </v-col>
